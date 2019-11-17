f2346c0eb871fc28f502b7172c25a828c732387f
Mamessile Kiman <sile.kiman@yahoo.com>
2019-11-17 00:25:29 -0500

    updating my schedule.js file with store value code

--- scheduler.js
+++ scheduler.js
@@ -3,150 +3,123 @@
 var dailyhours = ["9 AM", "10 AM", "11 AM", "12 PM", "1PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 var hourEL = document.querySelectorAll(".hour");
 var rowEl = document.querySelector(".row");
-var eventEl = document.querySelectorAll("textarea")
-var colHour = document.querySelector(".col")
+var eventEl = document.querySelectorAll("textarea");
+var colHour = document.querySelector(".col");
 var eventEl1 = document.querySelector("textarea").value
-
 var dateEl = document.querySelector("#currentDay");
+var btnEL = document.querySelectorAll(".saveBtn");
+
+//Setup date to display on the page
 var date = new Date();
 date = moment().format('LL');
-var date1 = moment().format("h A")
-console.log(date1)
 
-console.log(moment())
+//Setup time for daily event
+var currentTime = moment().format("h A")
+//console.log(currentTime)
+//console.log(moment())
 
 
 //Function to get the current date
 function getTodayDate() {
     dateEl.textContent = date;
-
-
 }
 getTodayDate();
 
+//Set an index to loop through the hours array
 var i = 0;
 
+//Function to display the hours and set difference color attribute to the textare dynamically 
 function outputHours() {
-    for (var j = 0; j < hourEL.length; j++) {
+    for (var j = 0; j< hourEL.length; j++) {
         if (i < dailyhours.length) {
-            var test = hourEL[j].textContent = dailyhours[i];
-            //var fiedlVal= eventEl[i].value
-            //fiedlVal = 9;
-            //console.log(fiedlVal)
+            hourEL[j].textContent = dailyhours[i];
+          
             i++
         }
-        if (dailyhours[i] > date1) {
+        if (dailyhours[i]>currentTime) {
 
             eventEl[i].setAttribute("class", "future")
             console.log(dailyhours[i])
-            alert("Future")
+            //alert("Future")
 
-        } else if (dailyhours[i] === date1) {
+        } else if (dailyhours[i]===currentTime) {
             eventEl[i].setAttribute("class", "present")
-            alert("Current")
-
+            //alert("Current")
         }
-        //saveEvent(event)
+        //renderLastItem()
     }
 }
+//Call outputHours function
 outputHours()
 
-//var i=0;
-var btnEL = document.querySelectorAll(".saveBtn");
 
-//var eventEl = document.querySelectorAll("textarea").values;
-//var eventEl1 = document.querySelector("textarea");
-//var textareaVal =eventEl[i];
-//function saveEvent(event){
-var eventEl = document.querySelectorAll("textarea");
-console.log(eventVal)
+//var eventEl = document.querySelectorAll("textarea");
+//console.log(eventVal)
+
+
+//Set some more variables to use in the SaveEvent funtions
 var e = 0;
 var eventVal = "";
 var eventKey="";
+
+//Function to save each even to the local storage. 
 function saveEvent() {
     if (e < eventEl.length) {
         eventVal = eventEl[e].value;
         eventKey= dailyhours[e];
-        
-        if (eventVal === "") {
-            //errMsg.setAttribute("style", "color: red")
-            //errMsg.textContent = "Text area cannot be empty when save button is clicked"
-            return
-        } else {
-            //textareaVal.textContent = "";
-            localStorage.getItem(eventVal)
-            localStorage.setItem(eventKey, eventVal)
-            renderLastItem()
+        renderLastItem()
+        if (eventVal==="") {
+             return
 
-            console.log(eventVal)
-        }
+        }else{
+             
+             localStorage.getItem(eventVal)
+             localStorage.setItem(eventKey, eventVal)
+             //renderLastItem()
+             //console.log(eventVal)
+            }
         //renderLastItem()
         e++;
-        //renderLastItem()
+        renderLastItem()
     }
     //renderLastItem()
 }
-var eventHolder="";
-/**function to show the last page  */
+ 
+
+/**function to render the last saved entry in the local storage into the textarea */
 function renderLastItem() {
     var lastEvent = localStorage.getItem(eventKey);
+    var textAreaVal= eventEl[e].value;
+    console.log(lastEvent)
     var eventHolder =+ lastEvent;
     if (lastEvent=== "") {
+
         return
-    }else{
-        eventEl[e].value=lastEvent;
+
+   }else{
+         
+        textAreaVal=lastEvent;
+        //dateEl.textContent=lastEvent
+        //console.log(textAreaVal)
     }
-}
-//renderLastItem()
-
-
-/**This function will refresh the page and send user back to begining page when go back button is clicked */
-function init() {
-    if(location.reload()===true)
-    //location.reload();
-    renderLastItem()
-
+   
 }
 
+renderLastItem()
+/**This function will refresh the page and  repopulate the events after refresh**/
+function init(){
+    location.reload()
+   // renderLastItem()
 
+}
+//call init function
+//init()
 
-
-//event.addEventListener("click",saveEvent)
-//});
-//var eventEl = document.querySelectorAll("textarea");
-console.log(eventEl)
-
-
-// var textareaVal =eventEl[i].value;
-// document.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     event.target
-//     var eventEL = document.querySelector("textarea").value;
-//     if (textareaVal === "") {
-//         //errMsg.setAttribute("style", "color: red")
-//         //errMsg.textContent = "Text area cannot be empty when save button is clicked"
-//         return
-//     } else {
-//         textareaVal.textContent = "";
-//         localStorage.getItem(textareaVal)
-//         localStorage.setItem("Event", textareaVal)
-//          //renderLastItem()
-
-//     }
-// })
-//  /**Function to display users final score */
-//  function displayResult() {
-//     finishDiv.style.visibility = "visible";
-//     timeElement.textContent = "Time:" + " " + timer;
-//     var HighScores = timer;
-//     localStorage.getItem(HighScores)
-//     finalScore.textContent = "Your finally score is: " + HighScores;
-//      localStorage.setItem("HighScores", HighScores)
-
-// }
-
+var btnEL = document.querySelectorAll(".saveBtn");
+//Add an EventListner to save the events into local storage on click
 btnEL.forEach(function (event) {
     event.addEventListener("click", saveEvent)
+    
 });
 
 
